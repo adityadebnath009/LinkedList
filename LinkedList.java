222c502344510478ed2f31a840e4d46998a296bc
Aditya Debnath <debnathaditya2005@gmail.com>
2024-10-01 12:27:31 +0000

    Done

---
+++ LinkedList.java
@@ -0,0 +1,36 @@
+class Node {
+    int data;         // Data stored in the node
+    Node next;        // Reference to the next node in the linked list
+    // Constructor
+    public Node(int data1, Node next1) {
+        this.data = data1;
+        this.next = next1;
+    }
+    // Constructor
+    public Node(int data1) {
+        this.data = data1;
+        this.next = null;
+    }
+}
+public class LinkedList{
+    private static Node ConvertArr2LL(int[] arr)
+    {
+        Node head = new Node(arr[0]);
+        Node mover = head;
+        for(int i=1;i<arr.length;i++)
+        {
+            Node temp = new Node(arr[i]);
+            mover.next = temp;
+            mover  = temp;
+        }
+
+        return head;
+
+    }
+    public static void main(String[] args) {
+        int[] arr = {2, 5, 8, 7};
+        Node head = ConvertArr2LL(arr);
+        System.out.println(head.data);
+        
+    }
+}
